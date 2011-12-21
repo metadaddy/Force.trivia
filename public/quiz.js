bf1de632193e078bb2cc458dc3d962f980ce2434
Pat Patterson <ppatterson@salesforce.com>
2011-12-20 17:05:56 -0800

    Save name/handle in cookies

--- public/quiz.js
+++ public/quiz.js
@@ -17,10 +17,6 @@ Quiz = {
         this._app    = $('#app');
         this._post   = $('#postMessage');
         this._stream = $('#stream');
-    
-        this._app.hide();
-    
-        $('#handle').focus();
         
         // Subscribe to the chat channel
         var subscription = self._client.subscribe('/quiz', self.accept, self);
@@ -29,21 +25,29 @@ Quiz = {
             alert("Error subscribing: " + error.message);
         });
         
-        // When the user enters handle/name, store them and start the app
-        this._login.submit(function() {
-          $('#error').empty();
-            if ( self._timeout ) {
-                window.clearTimeout(self._timeout);
-            }
-            self._timeout = setTimeout(function(){
-                self._timeout = null;
-                alert('No reply from the quizmaster - check that the quiz has started and try again!');
-            }, 10*1000);
-            self._handle = $('#handle').val();
-            self._name = $('#name').val();
+        self._handle = $.cookie('handle');
+        self._name   = $.cookie('name');
+        
+        if (self._handle && self._name) {
+            $('#handle').val(self._handle);
+            $('#name').val(self._name);
+            
             self.launch();
-            return false;
-        });
+        } else {
+            this._app.hide();
+
+            $('#handle').focus();
+
+            // When the user enters handle/name, store them and start the app
+            this._login.submit(function() {
+                self._handle = $('#handle').val();
+                self._name = $('#name').val();
+                $.cookie('handle', self._handle, { expires: 1 });
+                $.cookie('name', self._name, { expires: 1 });
+                self.launch();
+                return false;
+            });
+        }            
     },
   
     /**
@@ -54,6 +58,15 @@ Quiz = {
     launch: function() {
         var self = this;
     
+        $('#error').empty();
+        if ( self._timeout ) {
+            window.clearTimeout(self._timeout);
+        }
+        self._timeout = setTimeout(function(){
+            self._timeout = null;
+            alert('No reply from the quizmaster - check that the quiz has started and try again!');
+        }, 10*1000);
+        
         self._client.publish('/quiz', {
             handle: self._handle, 
             name: self._name, 
@@ -95,6 +108,12 @@ Quiz = {
                     $('#buzz').attr('disabled', 'disabled');
                     $('#buzz').attr('src', 'button-gray.png');
                     return false;
+                });
+                
+                $('#logout').click(function() {
+                    $.cookie('handle', null);
+                    $.cookie('name', null);
+                    window.location.reload();
                 });
             } else {
                 alert(html.escapeAttrib(message.error));
